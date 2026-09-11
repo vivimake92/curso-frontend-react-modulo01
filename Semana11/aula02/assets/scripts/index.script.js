01d68c9896756a5083169492aa99ba6362447821
vivimake92 <vivi.make92@gmail.com>
2026-09-11 19:30:15 -0300

    feat: Adiciona funcionalidade de limpeza do localStorage para o nome do visitante

--- Semana11/aula02/assets/scripts/index.script.js
+++ Semana11/aula02/assets/scripts/index.script.js
@@ -58,3 +58,12 @@ const savedUsers = JSON.parse(localStorage.getItem("usuarios-lista"));
 
 console.log(savedUsers);
 */
+
+const clearButton = document.getElementById("clear-button");
+
+clearButton.addEventListener("click", () => {
+  console.log("Limpeza localStorage");
+
+  // localStorage.clear();
+  localStorage.removeItem("visitante-nome");
+});
